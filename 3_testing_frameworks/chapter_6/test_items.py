8657826c7a2f791cd94221a5ca5e566c4c8152d7
Anton Moiseev <a.moiseev@softpro.com>
2019-06-27 15:42:55 +0300

    add test_items.py

--- 3_testing_frameworks/chapter_6/test_items.py
+++ 3_testing_frameworks/chapter_6/test_items.py
@@ -1,9 +1,5 @@
-import time
-
-
-def test_items(browser):
+def test_check_add_basket_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
 
     browser.find_element_by_css_selector('[id="add_to_basket_form"]')
-    time.sleep(3)
